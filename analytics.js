7d54ce49eed7165700ab25a62067c83d7c903631
flourcode <mark.flournoy@gmail.com>
2026-09-05 13:02:08 -0400

    Add files via upload

--- analytics.js
+++ analytics.js
@@ -2,7 +2,7 @@
    Replace G-XXXXXXXXXX below with your GA4 Measurement ID (Admin > Data streams > your web stream).
    That is the only edit needed; every page loads this file. Leave it as-is and nothing is sent. */
 (function(){
-  var ID="G-XXXXXXXXXX";
+  var ID="G-NY26QJW212";
   if(ID.indexOf("G-XXXX")===0)return;
   var s=document.createElement("script");s.async=true;s.src="https://www.googletagmanager.com/gtag/js?id="+ID;document.head.appendChild(s);
   window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}window.gtag=gtag;
